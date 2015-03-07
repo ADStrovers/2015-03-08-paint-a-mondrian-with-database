2ea7caaf4735c141cd65b56372460e199c5de4b8
Andrew Strovers <ADStrovers@gmail.com>
2015-03-07 15:41:43 -0600

    First commit..  Little late.  Working product but lack of styling.

---
+++ script.js
@@ -0,0 +1,30 @@
+var colorArray = [];
+var canvasArray = [];
+var pointerColor = "white";
+
+function setPointerColor() {
+  pointerColor = this.className
+}
+
+function setCanvasColor() {
+  this.style.backgroundColor = pointerColor;
+}
+
+window.onload = function() {
+  canvasArray = document.getElementsByClassName("box")
+  
+  var childrenOfColors = document.getElementsByClassName("colors")[0].childNodes;
+  for (i = 0; i < childrenOfColors.length; i++) {
+    if (childrenOfColors[i].tagName === "DIV") {
+      colorArray.push(childrenOfColors[i]);
+    };
+  };
+  
+  for (i = 0; i < colorArray.length; i++) {
+    colorArray[i].addEventListener("click", setPointerColor);
+  };
+  
+  for (i = 0; i < canvasArray.length; i++) {
+    canvasArray[i].addEventListener("click", setCanvasColor);
+  };
+};
