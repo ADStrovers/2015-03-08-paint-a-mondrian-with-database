b3e79383507a5f3a8f1a3db140508ed4cf4e111d
Andrew Strovers <ADStrovers@gmail.com>
2015-03-14 16:02:20 -0500

    First commit to branch.  Set up production and development in main.

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require 'pry'
-require 'sqlite3'
 require 'json'
 require 'open-uri'
 require 'sinatra/activerecord'
@@ -12,8 +11,24 @@ set :bind, '0.0.0.0'
 set :partial_template_engine, :erb
 enable :sessions
 
-DATABASE = SQLite3::Database.new("database.db")
-set :database, {adapter: "sqlite3", database: "database.db"}
+configure :development do
+  require 'sqlite3'
+  DATABASE = SQLite3::Database.new("database.db")
+  set :database, {adapter: "sqlite3", database: "database.db"}
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'])
+  
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
 
 Dir["./database/*.rb"].each{ |file| require file }
 Dir["./controllers/*.rb"].each{ |file| require file }
